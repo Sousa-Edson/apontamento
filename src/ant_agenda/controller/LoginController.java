3f4e8163d4cc224ae7c8c1ac5e953d47f5f5c67c
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-08-04 08:04:12 -0300

    inicianod projeto apontamento

---
+++ src/ant_agenda/controller/LoginController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ant_agenda.controller;
+
+import ant_agenda.view.LoginForm;
+import ant_agenda.view.PrincipalForm;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author hp
+ */
+public class LoginController {
+
+    private final LoginForm view;
+
+    int xx, xy;
+
+    public LoginController(LoginForm view) {
+        this.view = view;
+    }
+
+    public void teste() {
+        view.getTxtLogin().getText();
+        JOptionPane.showMessageDialog(view, "O TEXTO É: " + view.getTxtLogin().getText());
+    }
+
+    public void chamaPrincipal() {
+        PrincipalForm t = new PrincipalForm();
+        t.setVisible(true);
+        view.dispose();
+    }
+
+    public void localAtual(java.awt.event.MouseEvent evt) {
+        xx = evt.getX();
+        xy = evt.getY();
+    }
+
+    public void moveLocal(java.awt.event.MouseEvent evt) {
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        view.setLocation(x - xx, y - xy);
+    }
+}
